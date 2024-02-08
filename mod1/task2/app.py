782e5e163d3b4127b82f5fde8d9e69ed1b2f9be4
LyutinAl <AlexLiutin@outlook.com>
2024-02-08 17:26:35 +0500

    End first module

---
+++ mod1/task2/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+car_list = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+
+@app.route('/cars')
+def cars():
+    return ', '.join(car_list)
---
+++ mod1/task7/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import argparse
+
+app = Flask(__name__)
+
+visit_counter = 0
+
+@app.route('/counter')
+def counter():
+    global visit_counter
+    visit_counter += 1
+    return f'{visit_counter}'
+
+# Run this script as: `python .\app.py --port=5555` by using argparse
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Запуск Flask приложения')
+    parser.add_argument('--port', type=int, default=5000, help='Порт для запуска Flask приложения')
+    args = parser.parse_args()
+
+    app.run(debug=True, port=args.port)
