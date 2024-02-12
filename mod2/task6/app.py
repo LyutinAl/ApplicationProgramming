d77bbe0536fce81d51b36f2e97272a9fc6348913
LyutinAl <AlexLiutin@outlook.com>
2024-02-12 09:13:46 +0500

    Finish mod2

--- mod2/task6/app.py
+++ mod2/task6/app.py
@@ -7,7 +7,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/preview/<size>/<path:rel_path>')
 def preview(size, rel_path):
-    abs_path = BASE_DIR + '/' + rel_path
-    with open(abs_path, 'r') as file:
-        result_text = file.read(int(size))
-    return f'Size is: {size}\nAbs. path is: {abs_path} \\\\\\\\{result_text}'
+    abs_path = os.path.abspath(os.path.join(BASE_DIR, rel_path))
+    try:
+        with open(abs_path, 'r') as file:
+            result_text = file.read(size)
+    except FileNotFoundError:
+        return 'Ошибка при открытии, файл не найден', 403
+    except Exception as e:
+        return f'Ошибка при открытии файла: {e}', 400
+
+    return (f'<b>{abs_path}</b> {len(result_text)}<br>'
+            f'{result_text}')
---
+++ mod2/task7/test.sh
@@ -0,0 +1,3 @@
+xdg-open http://127.0.0.1:5555/add/20220912/500
+python ./app.py --port=5555
+#python -m flask run --port=5555
